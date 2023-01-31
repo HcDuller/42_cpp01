98940a39fcdb562491084759634f4e5f3730e856
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-01-31 16:19:06 -0300

    added const to getType()

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,14 +6,14 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 14:35:55 by hde-camp          #+#    #+#             */
-/*   Updated: 2023/01/26 15:27:37 by hde-camp         ###   ########.fr       */
+/*   Updated: 2023/01/31 15:58:10 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include "./Weapon.hpp"
 
-std::string& Weapon::getType(){
+const std::string& Weapon::getType(){
 	std::string& typeRef = this->type;
 	return (typeRef);
 }
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 14:22:43 by hde-camp          #+#    #+#             */
-/*   Updated: 2023/01/26 15:27:14 by hde-camp         ###   ########.fr       */
+/*   Updated: 2023/01/31 15:58:17 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ class Weapon
 private:
 	std::string type;
 public:
-	std::string&	getType();
+	const std::string&	getType();
 	void			setType(std::string newType);
 	Weapon(std::string type);
 	Weapon();
